77332faff5931851853dcf462984b05af65d803c
DmytroPazhytniev <pajitnev.dmitro@gmail.com>
2026-04-09 14:06:46 +0300

    начальная миграция SqliteInitial для создания таблиц базы данных

--- obj/Debug/net10.0/MyApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1558ef9faeb11047de32d47e47f0cdcc5efdbac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12c0495954925831de4c4bbd7d8602ccd7f6ae46")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
